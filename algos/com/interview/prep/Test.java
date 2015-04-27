256f4d785ad01e0f927f9b225673bc7a10c217ad
vishwajan17 <vishwajan@VishwanathasMBP.home>
2015-04-27 01:17:33 -0400

    Initial Setup

---
+++ algos/com/interview/prep/Test.java
@@ -0,0 +1,11 @@
+package com.interview.prep;
+
+/**
+ * Created by vishwajan on 4/27/15.
+ */
+public class Test
+{
+    public static void main(String[] args) {
+        System.out.println("Hello Vishu!");
+    }
+}
